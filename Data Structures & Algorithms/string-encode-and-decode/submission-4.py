2c6a17dd06597255aa902ed3b9b32dd7d3088f31
Shinsha <137866950+shinshARK@users.noreply.github.com>
2026-03-31 12:39:21 +0700

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for string in strs:
+            encoded += f"{len(string)}#{string}"
+        print(encoded)
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        print(s)
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                print(f"s[j-1] is {s[j-1]} isdigit: {s[j-1].isdigit()}")
+                j+=1
+            
+            length = int(s[i:j])
+            res.append(s[j + 1: j + 1 + length])
+            i = j + 1 + length
+        return res
